a5f7770b07b0f364d04f2b251654fd8dfefb9a5c
travelingit <stevendek@hotmail.com>
2018-05-25 14:22:24 +0200

    Main Modules created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: "game", component: GameComponent},
+      {path: "home", component: HomeComponent},
+      {path:"", redirectTo: "home", pathMatch:'full'}
+    ], {useHash:true})
+
   ],
   providers: [],
   bootstrap: [AppComponent]
